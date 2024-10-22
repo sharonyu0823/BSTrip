f3ac9ed6e1d25c39d6df19a781df5a171b0e2ee8
sharon <>
2024-10-22 22:03:01 +0800

    install taliwnd

---
+++ my-project/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='container border-black border-2'>
+      <div className='h-1 mt-15 border-orange border-2'></div>
+      <div className='h-48 mt-15 border-orange border-2'></div>
+      <div className='h-48 mt-15 border-orange border-2'></div>
+
+      {/* <h1 className='text-3xl font-bold bg-orange mt-12'>Tailwind CSS</h1>
+      <div className="text-3xl font-bold underline">Hello world!</div> */}
+    </div>
+    </>
+    
+  )
+}
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+export default App
---
+++ my-project/tailwind.config.js
@@ -0,0 +1,61 @@
+/** @type {import('tailwindcss').Config} */
+
+// module.exports = {
+//   theme: {
+//     screens: {
+//       sm: '480px',
+//       md: '768px',
+//       lg: '976px',
+//       xl: '1440px',
+//     },
+//     colors: {
+//       'blue': '#1fb6ff',
+//       'purple': '#7e5bef',
+//       'pink': '#ff49db',
+//       'orange': '#ff7849',
+//       'green': '#13ce66',
+//       'yellow': '#ffc82c',
+//       'gray-dark': '#273444',
+//       'gray': '#8492a6',
+//       'gray-light': '#d3dce6',
+//     },
+//     fontFamily: {
+//       sans: ['Graphik', 'sans-serif'],
+//       serif: ['Merriweather', 'serif'],
+//     },
+//     extend: {
+//       spacing: {
+//         '128': '32rem',
+//         '144': '36rem',
+//       },
+//       borderRadius: {
+//         '4xl': '2rem',
+//       }
+//     }
+//   }
+// }
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      // padding: '1.5rem',
+    },
+    colors: {
+      'orange': '#F2784B',
+      'orange-bright': '#F25922',
+      'green-bright': '#32A69A',
+      'black': '#000000',
+      'white': '#FFFFFF',
+      'gray-light': '#F5F5F5',
+      'coffee': '#AE9393',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
