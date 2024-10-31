54e5ad21b47f1ed774e246cd8c2f407f968ec5e3
Sharon Yu <yu5286pp@gmail.com>
2024-10-31 21:03:50 +0800

    完成routing

---
+++ my-project/src/pages/Itinerary/ItineraryAdd.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faCircleCheck, faCircle, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+export default class ItineraryAdd extends Component {
+  render() {
+    return (
+        // == 新增行程頁 start ==
+        <>
+        <div className="container h-screen">
+            <div className="h-12 py-2 pl-5 bg-orange-bright flex items-center">
+            <div className="text-black"><FontAwesomeIcon icon={faChevronLeft} /></div>
+            <div className='w-full text-18px font-bold text-white text-center -translate-x-2.5'>
+                新增
+            </div>
+            </div>
+            <div className="h-[calc(100vh-112px)] p-5 flex flex-col space-y-4">
+            <div>
+                <div className="text-16px mb-4">地點名稱</div>
+                <div className="bg-pink-light rounded-md">
+                <div className='flex justify-between justify-center p-5'>
+                    <div className='text-14px text-center'>請輸入地點名稱</div>
+                </div>
+                </div>
+            </div>
+            <div>
+                <div className="text-16px mb-4">抵達時間</div>
+                <div className="bg-pink-light rounded-md flex flex-col">
+                <div className='flex justify-between justify-center p-5 border-b-2 border-coffee border-dotted'>
+                    <div className='text-14px w-20 text-center'>系統規劃</div>
+                    <div className='text-14px w-2/4 text-center -translate-x-2.5'>08:00</div>
+                    <div className='text-14px w-1/4 text-center'><FontAwesomeIcon icon={faCircleCheck} /></div>
+                </div>
+                <div className='flex justify-between justify-center p-5'>
+                    <div className='text-14px w-20 text-center text-coffee pt-1'>手動設定</div>
+                    <div className='text-14px w-1/3 text-center border-2 border-coffee flex flex-row justify-center justify-around rounded-full py-1 px-4'>
+                    <div className='w-2/3'>18:00</div>
+                    <div className='w-1/3'><FontAwesomeIcon icon={faChevronDown} /></div>
+                    </div>
+                    <div className='text-14px w-1/4 text-center text-white pt-1'><FontAwesomeIcon icon={faCircle} /></div>
+                </div>
+                </div>
+            </div>
+            <div>
+                <div className="text-16px mb-4">此景點</div>
+                <div className="bg-pink-light rounded-md flex flex-col">
+                <div className='flex justify-between justify-center p-5 border-b-2 border-coffee border-dotted'>
+                    <div className='text-14px w-20 text-center translate-y-4'>停留時間</div>
+                    <div className='text-14px w-1/3 text-center border-2 border-coffee flex flex-row justify-center justify-around rounded-full py-1 px-4'>
+                    <div className='w-2/3'>00時00分</div>
+                    <div className='w-1/3 translate-y-2.5'><FontAwesomeIcon icon={faChevronDown} /></div>
+                    </div>
+                    <div className='text-14px w-1/4 text-center translate-y-4'><FontAwesomeIcon icon={faCircleCheck} /></div>
+                </div>
+                <div className='flex justify-between justify-center p-5'>
+                    <div className='text-14px w-20 text-center text-coffee pt-1'>離開時間</div>
+                    <div className='text-14px w-1/3 text-center border-2 border-coffee flex flex-row justify-center justify-around rounded-full py-1 px-4'>
+                    <div className='w-2/3'>18:00</div>
+                    <div className='w-1/3'><FontAwesomeIcon icon={faChevronDown} /></div>
+                    </div>
+                    <div className='text-14px w-1/4 text-center text-white pt-1'><FontAwesomeIcon icon={faCircle} /></div>
+                </div>
+                </div>
+            </div>
+            </div>
+            <div className="h-16 flex justify-center px-5 pb-5">
+            <div className="text-18px text-center rounded-full text-white bg-orange py-3.5 w-full h-12">儲存</div>
+            </div>
+        </div>
+        </>
+        // == 新增行程頁 end ==
+    )
+  }
+}
---
+++ my-project/src/pages/Questionnaire/Questionnaire.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+
+export default class Questionnaire extends Component {
+  render() {
+    return (
+        // == 問券頁 start ==
+        <>
+          <div className="container h-screen">
+            <div className="h-12 flex flex-row-reverse items-center">
+              <p className="w-fit mr-4 text-12px font-bold">略過</p>
+            </div>
+            <div className="h-[calc(100vh-112px)] p-5 flex flex-col space-y-4">
+              <div className="flex flex-col">
+                <div className="text-20px font-bold pr-1.5">歡迎使用BSTrip</div>
+                <div className="text-14px text-coffee">寫基本設定，推薦內容更精準!</div>
+              </div>
+              <div className="flex flex-col">
+                <div>
+                  <div className="text-16px mb-4">生日</div>
+                  <div className="border-coffee border-2 mb-4 rounded-md flex flex-row justify-between">
+                    <div className="text-14px p-2.5 text-coffee">選擇生日</div>
+                    <div className="text-coffee p-2.5"><FontAwesomeIcon icon={faAngleDown} /></div>
+                  </div>
+                </div>
+                <div>
+                  <div className="text-16px mb-4">性別</div>
+                  <div className="flex justify-between mb-4">
+                    <div className="text-14px text-center border-coffee flex-1 mr-3 border-2 p-2.5 rounded-md text-coffee">男</div>
+                    <div className="text-14px text-center border-coffee flex-1 mr-3 border-2 p-2.5 rounded-md text-coffee">女</div>
+                    <div className="text-14px text-center border-coffee flex-1 border-2 p-2.5 rounded-md text-coffee">秘密</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="h-16 flex justify-center px-5 pb-5">
+            <div className="text-18px text-center rounded-full text-white bg-orange py-3.5 w-full h-12">下一步</div>
+            </div>
+          </div>
+        </>
+        // == 問券頁 end ==
+    )
+  }
+}
